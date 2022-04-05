85ac041cd63358849eee5cfe70235fbc279b9ff6
Mithaecus <hey@eddy.ninja>
2022-04-05 12:28:32 -0700

    Add mangata-finance system install script

---
+++ mangata-finance/node-dependencies.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(require racket/system)
+
+(system "curl https://getsubstrate.io -sSf | bash -s -- --fast")
+(system "source ~/.cargo/env ")
+(system "curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain none")
+(system "rustup default nightly-2021-10-19")
+(system "rustup target add wasm32-unknown-unknown")
