8c5cb778f4ed7a4338b4d8fe3239b89fa9e6f767
Mithaecus <hey@eddy.ninja>
2022-04-07 09:15:55 -0700

    Handle read errors on missing pages

--- data-crawling/flavor-db.rkt
+++ data-crawling/flavor-db.rkt
@@ -1,21 +1,22 @@
 #lang racket
 
-(require net/http-client json)
+(require json net/http-client)
 
-(for ([i (in-range 1 1000)])
-    (define-values (status header response)
-    (http-sendrecv 
-        "cosylab.iiitd.edu.in" 
-        (string-append "/flavordb/entities_json?id=" (number->string i))
-         #:ssl? #t))
-    (define data (read-json response))
+(for ([i (in-range 405 1000)])
+    (with-handlers ([exn:fail:read? (λ (exn) 'ignore-missing-pages-error)])
+        (define-values (status header response)
+        (http-sendrecv 
+            "cosylab.iiitd.edu.in" 
+            (string-append "/flavordb/entities_json?id=" (number->string i))
+            #:ssl? #t))
+        (define data (read-json response))
 
-    (jsexpr->string data)
+        (jsexpr->string data)
 
-    (define (write-json-wrapper jsexpr filename)
-    (call-with-output-file filename
-        (λ (x) (write-json jsexpr x))
-        #:exists 'replace))
+        (define (write-json-wrapper jsexpr filename)
+        (call-with-output-file filename
+            (λ (x) (write-json jsexpr x))
+            #:exists 'replace))
 
-    (write-json-wrapper data 
-        (string-append "./" (number->string (hash-ref data 'entity_id)) ".json")))
+        (write-json-wrapper data 
+            (string-append "./" (number->string (hash-ref data 'entity_id)) ".json"))))
